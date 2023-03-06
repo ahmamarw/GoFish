cb2b9266c6a81aa7021d440a94db31855264914c
ahmamarw <ahmamarw@sheridancollege.ca>
2023-03-05 21:56:43 -0500

    GoFish card Game

---
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -0,0 +1,53 @@
+package ca.sheridancollege.project;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
+ * once. The group of cards has a maximum size attribute which is flexible for reuse.
+ * @author Marwa    5 Mar 2023
+ * @author Sumandeep
+ * @author Jashman
+ * @author Shubhdeep
+ * @author Manvir
+ */
+public class GroupOfCards {
+
+    //The group of cards, stored in an ArrayList
+    private ArrayList<Card> Cards;
+    private int size;//the size of the grouping
+
+    public GroupOfCards(int size) {
+        this.size = size;
+    }
+
+    /**
+     * A method that will get the group of cards as an ArrayList
+     *
+     * @return the group of cards.
+     */
+    public ArrayList<Card> getCards() {
+        return Cards;
+    }
+
+    public void shuffle() {
+        Collections.shuffle(Cards);
+    }
+
+    /**
+     * @return the size of the group of cards
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the max size for the group of cards
+     */
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+}//end class
+
